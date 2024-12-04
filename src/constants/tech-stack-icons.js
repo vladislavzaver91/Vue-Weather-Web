84cba5e9f12035bce65444bd79ba446443f0f24e
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-12-04 17:24:26 +0200

    fix: fixed icon`s path

--- src/constants/tech-stack-icons.js
+++ src/constants/tech-stack-icons.js
@@ -1,6 +1,6 @@
 export const TECH_STACK_ICONS = [
-  { name: 'Vue.js', path: '/src/assets/icons/Vue.svg' },
-  { name: 'Pinia', path: '/src/assets/icons/Pinia.svg' },
-  { name: 'Sass/Scss', path: '/src/assets/icons/Sass.svg' },
-  { name: 'Openweather API', path: '/src/assets/icons/Open-weather-api.svg' },
+  { name: 'Vue.js', path: '/icons/Vue.svg' },
+  { name: 'Pinia', path: '/icons/Pinia.svg' },
+  { name: 'Sass/Scss', path: '/icons/Sass.svg' },
+  { name: 'Openweather API', path: '/icons/Open-weather-api.svg' },
 ]
